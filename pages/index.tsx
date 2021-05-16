bb17944ce50dbf12e42de4659f9c4f797cfe716e
Yusuke Nakanishi <yn4k4nishi.kit@gmail.com>
2021-05-16 22:30:09 +0900

    :+1: Update : textarea tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Editor(){
         <div className="editor flex-grow flex-shrink">
           <form className="h-full">
             <textarea
-              type="text"
+              name="post-title"
               id="post-title"
               placeholder="Title"
               className="px-5 block mx-auto w-4/5 rounded-lg border-0 h-14 text-2xl font-mono font-bold focus:outline-none mb-0 resize-none"
